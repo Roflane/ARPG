d2c23c79628512d309fd7af33ea552c9f984fd3d
K1n <69646118+Roflane@users.noreply.github.com>
2025-06-23 17:59:33 +0400

    final

--- Assets/Scripts/Settings/KeyBinds.cs
+++ Assets/Scripts/Settings/KeyBinds.cs
@@ -3,9 +3,9 @@
 public static class KeyBinds {
     public static readonly KeyCode BasicAttack = KeyCode.K;
     public static readonly KeyCode Blink = KeyCode.LeftShift;
-    public static readonly KeyCode Fire = KeyCode.M;
-    public static readonly KeyCode Ice = KeyCode.I;
-    public static readonly KeyCode Lightning = KeyCode.J;
+    public static readonly KeyCode Fire = KeyCode.J;
+    public static readonly KeyCode Lightning = KeyCode.I;
+    public static readonly KeyCode Ice = KeyCode.L;
     public static readonly KeyCode Stats = KeyCode.Escape; 
     public static readonly KeyCode SkillTree = KeyCode.T; 
 }
--- Assets/Scripts/Skills/Blink.cs
+++ Assets/Scripts/Skills/Blink.cs
@@ -8,27 +8,25 @@ public class Blink : MonoBehaviour {
     public Transform player;
     public Image blinkImage;
     public TMP_Text blinkTimerText;
-    
+
     void Update() {
         _blinkTimer -= Time.deltaTime;
         CastBlink();
         UpdateBlinkIcon();
         UpdateBlinkTimerText();
     }
-
+    
     private void CastBlink() {
         float blinkDistance = PlayerStatsManager.Instance.facingDirection * PlayerStatsManager.Instance.blinkDistance;
         if (Input.GetKeyDown(KeyBinds.Blink) && _blinkTimer <= 0) {
-            player.transform.position = new Vector3(player.transform.position.x + blinkDistance, player.transform.position.y, player.transform.position.z);
+            player.transform.position = new Vector2(player.transform.position.x + blinkDistance, player.transform.position.y);
             _blinkTimer = PlayerCooldown.Blink;
         }
     }
 
     private void UpdateBlinkIcon() {
-        if (_blinkTimer <= 0) {
-            blinkImage.color = Color.white;
-        }
-        else blinkImage.color = new (1f, 1f, 1f, 0.5f);
+        if (_blinkTimer <= 0) blinkImage.color = Color.white;
+        else blinkImage.color = new(1f, 1f, 1f, 0.5f);
     }
 
     private void UpdateBlinkTimerText() {
